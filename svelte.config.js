ed5c2b189683d41718e4b801d4415d016a68b13d
Pairadux <austincgause@gmail.com>
2025-01-29 21:25:54 -0500

    Style: format files

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    preprocess: vitePreprocess(),
 
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			fallback: '404.svelte',
-		}),
-    // paths: {
-    //   base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    // }
-	}
+    kit: {
+        adapter: adapter({
+            fallback: "404.svelte",
+        }),
+        //paths: {
+        //    base: process.argv.includes("dev") ? "" : "",
+        //},
+    },
 };
 
 export default config;

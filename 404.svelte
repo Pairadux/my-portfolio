db9306edca7e8368c60d51f3059b2e9b888a7f1f
Pairadux <austincgause@gmail.com>
2025-02-11 15:45:59 -0500

    Updates

--- 404.svelte
+++ 404.svelte
@@ -40,12 +40,12 @@
             href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap"
             rel="stylesheet" />
         
-		<link rel="modulepreload" href="/_app/immutable/entry/start.7lMzmijZ.js">
-		<link rel="modulepreload" href="/_app/immutable/chunks/entry.GkHOf1VL.js">
+		<link rel="modulepreload" href="/_app/immutable/entry/start.eMatvHqO.js">
+		<link rel="modulepreload" href="/_app/immutable/chunks/entry.cBiEYQ6g.js">
 		<link rel="modulepreload" href="/_app/immutable/chunks/scheduler.EKNBkY9R.js">
-		<link rel="modulepreload" href="/_app/immutable/chunks/paths.ZUd8DL8F.js">
+		<link rel="modulepreload" href="/_app/immutable/chunks/paths.a63Kqm_0.js">
 		<link rel="modulepreload" href="/_app/immutable/chunks/control.pJ1mnnAb.js">
-		<link rel="modulepreload" href="/_app/immutable/entry/app.486rGOxf.js">
+		<link rel="modulepreload" href="/_app/immutable/entry/app.eVmbmW87.js">
 		<link rel="modulepreload" href="/_app/immutable/chunks/preload-helper.0HuHagjb.js">
 		<link rel="modulepreload" href="/_app/immutable/chunks/index.uZSauDXW.js">
     </head>
@@ -55,15 +55,15 @@
         <div style="display: contents">
 			<script>
 				{
-					__sveltekit_17a5blh = {
+					__sveltekit_1q9aku0 = {
 						base: ""
 					};
 
 					const element = document.currentScript.parentElement;
 
 					Promise.all([
-						import("/_app/immutable/entry/start.7lMzmijZ.js"),
-						import("/_app/immutable/entry/app.486rGOxf.js")
+						import("/_app/immutable/entry/start.eMatvHqO.js"),
+						import("/_app/immutable/entry/app.eVmbmW87.js")
 					]).then(([kit, app]) => {
 						kit.start(app, element);
 					});
--- _app/immutable/entry/start.7lMzmijZ.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.GkHOf1VL.js";export{t as start};

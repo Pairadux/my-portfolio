a5da70ce5ef9f5411dc21b042390d32259ee20ac
Pairadux <austincgause@gmail.com>
2025-03-18 22:55:31 -0400

    Updates

---
+++ _app/immutable/entry/start._PsRPf50.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/DxJuJYGd.js";export{t as start};
---
+++ _app/immutable/nodes/3.CkgXvxBe.js
@@ -0,0 +1,47 @@
+import{s as J,n as O}from"../chunks/CT9ImElk.js";import{S as Q,i as Y,e as r,s as p,k as K,l as X,c as m,d as t,f as d,a as Z,m as ee,n as E,h as te,g as n,o as ne,p as ie,q as ae,r as oe,u}from"../chunks/DOYMmrxz.js";import{a as S}from"../chunks/DOK8GCST.js";import{P as se}from"../chunks/B16bPB-4.js";function le(G){let o,f='The Amazing World of <span aria-hidden="true" data-nosnippet=""><strike class="decoration-4 decoration-red-500">Gumball</strike></span> Austin',c,s,h=`Greetings, earthlings 🖖! I'm Austin, a Computer Science major,
+            network student assistant, and freelance web developer at Valdosta 
+            State University. Some of my passions include programming, web development,
+            Linux, and command-line applications. Whether it's building responsive websites,
+            crafting efficient CLI tools, or exploring the intricacies of the Linux ecosystem,
+            I'm always eager to dive into new challenges and expand my skill set.`,a,l,g=`However, my journey into technology began long before I ever wrote a
+            line of code. Even as a child, I was captivated by the mechanics of
+            everyday devices and how they operated. Around the age of 10, I
+            started taking apart electronics, driven by a curiosity to
+            understand what made them "tick." This fascination with technology
+            and problem-solving laid the foundation for my career today. While I
+            spent some time as a flooring installer, learning the value of
+            precision and hard work, my passion for tech has always been at the
+            core of my personal and professional growth. Now, I channel that
+            same curiosity and attention to detail into programming, web
+            development, and exploring the command line.`,T,v,W=`<img src="${S}/images/about/people-renovating-house-concept.jpg" alt="From Flooring to Coding"/> <figcaption>From Flooring to Coding: Building Foundations</figcaption>`,L,y,R=`My interest in programming began during high school when I first
+            attempted to write code in Python. Despite my enthusiasm, I
+            struggled to grasp many concepts due to the lack of formal teaching.
+            I spent countless hours on YouTube tutorials, managing to build
+            small apps and simple scripts but never quite feeling confident. It
+            wasn’t until the summer before my freshman year that I decided to
+            dive into web development, teaching myself the basics to gain a
+            "head start." However, it was in my CS 1010 class during freshman
+            year that everything truly clicked. With structured guidance and
+            formal instruction, programming finally started to make sense,
+            propelling me into exploring languages like Java and Rust, as well
+            as web development frameworks like Svelte and Tailwind CSS. Today,
+            as a freelance web developer and Network Student Assistant, I
+            continue to expand my skills, delving into both development and
+            networking fundamentals.`,$,w,q=`<img src="${S}/images/about/html-css-collage-concept-with-person.jpg" alt="Deep in Code"/> <figcaption>Lost in Lines of Code</figcaption>`,M,b,U=`Beyond the screen, I'm drawn to exploring a variety of interests. I
+            have a genuine love for foreign languages and cultures, along with a
+            deep appreciation for TV, music, gaming, and reading. Building
+            computers has also become an interesting, hands-on hobby, letting me
+            combine my technical skills with my craftsmanship. Gaming,
+            especially, is a big passion, and it often fuels my curiosity and
+            excitement for exploring new tech.`,H,x,V=`<img src="${S}/images/about/gaming.jpg" alt="Immersed in Gaming"/> <figcaption>Immersed in Gaming</figcaption>`,A,C,z=`I’m also active on campus as a member of the Association of
+            Computing Machinery (ACM), where I previously served as Treasurer. I
+            regularly attend on-campus events and occasionally volunteer on
+            campus. Volunteering has given me the chance to connect with others
+            and contribute to a supportive community.`,j,_,B=`<img src="${S}/images/about/different-people-doing-volunteer-work-with-food.jpg" alt="ACM Volunteering"/> <figcaption>Community First: Volunteering with ACM</figcaption>`,F,I,N=`When I'm not coding or studying, you might find me experimenting
+            with new CLI applications, working on personal projects, or
+            researching emerging technologies. I'm always eager to tackle new
+            challenges, collaborate on exciting projects, and continue growing
+            both personally and professionally.`,P,k,D=`Feel free to reach out if you&#39;d like to collaborate, chat about
+            tech, or <a href="https://www.youtube.com/watch?v=jabH3wRwiWQ" class="underline italic text-blue-500">discuss the political and economic state of the world right
+                now.</a>
+            Let&#39;s build something amazing together!`;return{c(){o=r("h2"),o.innerHTML=f,c=p(),s=r("p"),s.textContent=h,a=p(),l=r("p"),l.textContent=g,T=p(),v=r("figure"),v.innerHTML=W,L=p(),y=r("p"),y.textContent=R,$=p(),w=r("figure"),w.innerHTML=q,M=p(),b=r("p"),b.textContent=U,H=p(),x=r("figure"),x.innerHTML=V,A=p(),C=r("p"),C.textContent=z,j=p(),_=r("figure"),_.innerHTML=B,F=p(),I=r("p"),I.textContent=N,P=p(),k=r("p"),k.innerHTML=D},l(e){o=m(e,"H2",{"data-svelte-h":!0}),u(o)!=="svelte-kz6k57"&&(o.innerHTML=f),c=d(e),s=m(e,"P",{"data-svelte-h":!0}),u(s)!=="svelte-crh4gx"&&(s.textContent=h),a=d(e),l=m(e,"P",{"data-svelte-h":!0}),u(l)!=="svelte-1qs912m"&&(l.textContent=g),T=d(e),v=m(e,"FIGURE",{"data-svelte-h":!0}),u(v)!=="svelte-49o79f"&&(v.innerHTML=W),L=d(e),y=m(e,"P",{"data-svelte-h":!0}),u(y)!=="svelte-17wbv0m"&&(y.textContent=R),$=d(e),w=m(e,"FIGURE",{"data-svelte-h":!0}),u(w)!=="svelte-15eq7uj"&&(w.innerHTML=q),M=d(e),b=m(e,"P",{"data-svelte-h":!0}),u(b)!=="svelte-jalxum"&&(b.textContent=U),H=d(e),x=m(e,"FIGURE",{"data-svelte-h":!0}),u(x)!=="svelte-hx25e3"&&(x.innerHTML=V),A=d(e),C=m(e,"P",{"data-svelte-h":!0}),u(C)!=="svelte-1ihlxed"&&(C.textContent=z),j=d(e),_=m(e,"FIGURE",{"data-svelte-h":!0}),u(_)!=="svelte-2vrgnn"&&(_.innerHTML=B),F=d(e),I=m(e,"P",{"data-svelte-h":!0}),u(I)!=="svelte-i1wpdo"&&(I.textContent=N),P=d(e),k=m(e,"P",{"data-svelte-h":!0}),u(k)!=="svelte-1lc23e6"&&(k.innerHTML=D)},m(e,i){n(e,o,i),n(e,c,i),n(e,s,i),n(e,a,i),n(e,l,i),n(e,T,i),n(e,v,i),n(e,L,i),n(e,y,i),n(e,$,i),n(e,w,i),n(e,M,i),n(e,b,i),n(e,H,i),n(e,x,i),n(e,A,i),n(e,C,i),n(e,j,i),n(e,_,i),n(e,F,i),n(e,I,i),n(e,P,i),n(e,k,i)},p:O,d(e){e&&(t(o),t(c),t(s),t(a),t(l),t(T),t(v),t(L),t(y),t($),t(w),t(M),t(b),t(H),t(x),t(A),t(C),t(j),t(_),t(F),t(I),t(P),t(k))}}}function re(G){let o,f,c,s,h;return s=new se({props:{$$slots:{default:[le]},$$scope:{ctx:G}}}),{c(){o=r("meta"),f=p(),c=r("section"),K(s.$$.fragment),this.h()},l(a){const l=X("svelte-98br9s",document.head);o=m(l,"META",{name:!0,content:!0}),l.forEach(t),f=d(a),c=m(a,"SECTION",{class:!0});var g=Z(c);ee(s.$$.fragment,g),g.forEach(t),this.h()},h(){document.title="Austin Gause | About",E(o,"name","description"),E(o,"content","The Amazing World of Austin"),E(c,"class","mb-16")},m(a,l){te(document.head,o),n(a,f,l),n(a,c,l),ne(s,c,null),h=!0},p(a,[l]){const g={};l&1&&(g.$$scope={dirty:l,ctx:a}),s.$set(g)},i(a){h||(ie(s.$$.fragment,a),h=!0)},o(a){ae(s.$$.fragment,a),h=!1},d(a){a&&(t(f),t(c)),t(o),oe(s)}}}class de extends Q{constructor(o){super(),Y(this,o,null,re,J,{})}}export{de as component};

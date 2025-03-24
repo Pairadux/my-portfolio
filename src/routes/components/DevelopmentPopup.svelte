a8056d7cebb3502ca4c3ae92501b6b2f7d562d35
Pairadux <austincgause@gmail.com>
2025-03-24 10:00:26 -0400

    Style: format

--- src/routes/components/DevelopmentPopup.svelte
+++ src/routes/components/DevelopmentPopup.svelte
@@ -2,14 +2,25 @@
     import { assets } from "$app/paths";
 </script>
 
-<dialog open class="fixed top-0 left-0 h-screen w-screen z-50">
-    <div class="flex items-center justify-center flex-col h-full w-full dark:bg-black bg-white">
-        <img src="{ assets }/icons/alert.svg" alt="" class="h-16">
+<dialog
+    open
+    class="fixed left-0 top-0 z-50 h-screen w-screen">
+    <div class="flex h-full w-full flex-col items-center justify-center bg-white dark:bg-black">
+        <img
+            src="{assets}/icons/alert.svg"
+            alt=""
+            class="h-16" />
         <h2 class="text-2xl dark:text-white">This site is a work in progress</h2>
         <p class="w-3/4 text-center dark:text-white">If you wish to proceed, please click the button below.</p>
-        <p class="w-3/4 text-center dark:text-white">Wondering what's left? Check the <a target="_blank" href="https://github.com/users/Pairadux/projects/3" class="underline italic text-blue-500">roadmap</a>.</p>
+        <p class="w-3/4 text-center dark:text-white">
+            Wondering what's left? Check the <a
+                target="_blank"
+                href="https://github.com/users/Pairadux/projects/3"
+                class="italic text-blue-500 underline">roadmap</a
+            >.
+        </p>
         <form method="dialog">
-            <button class="p-2 mt-2 bg-black dark:bg-white text-white dark:text-black rounded-lg">Proceed</button>
+            <button class="mt-2 rounded-lg bg-black p-2 text-white dark:bg-white dark:text-black">Proceed</button>
         </form>
     </div>
 </dialog>

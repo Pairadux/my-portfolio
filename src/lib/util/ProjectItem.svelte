fb9c0d155b64567a720d3d7c7b984ef3f728143c
Pairadux <dev@austingause.com>
2026-02-16 07:46:18 -0500

    style: adjust project item styling

--- src/lib/util/ProjectItem.svelte
+++ src/lib/util/ProjectItem.svelte
@@ -8,10 +8,10 @@
 <a href="/projects/{slug}" class="no-underline">
     <div class="relative h-72 w-72 group transition-transform hover:transform hover:-translate-x-1 hover:-translate-y-1">
 
-        <div class="absolute group-hover:left-4 group-hover:top-4 transition-all left-2 top-2 h-[95%] w-[95%] outline-dashed outline-offset-2 outline-blue-500"></div>
+        <div class="absolute group-hover:left-4 group-hover:top-4 transition-all left-2 top-2 h-[95%] w-[95%] outline-dashed outline-offset-2 outline-[4px] outline-blue-500"></div>
 
         <!-- PORTFOLIO ITEMS -->
-        <div class="relative flex h-72 w-72 justify-center border-[1.5px] border-black dark:border-white bg-white shadow-md">
+        <div class="relative flex h-72 w-72 justify-center border-[1.5px] border-blue-500 bg-white shadow-frame">
 
             <!-- PORTFOLIO HOVER ITEMS -->
             <div class="absolute left-1/2 top-1/2 flex h-full w-full bg-blue-500 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 px-4 opacity-0 group-hover:opacity-100 transition-opacity">

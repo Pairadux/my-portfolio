e3559661a7a8ef28abd3ff29279834b11952e713
Pairadux <dev@austingause.com>
2026-02-16 16:37:44 -0500

    feat: dynamically render muxly and lapse to allow for text logo

--- src/lib/util/ProjectItem.svelte
+++ src/lib/util/ProjectItem.svelte
@@ -5,17 +5,17 @@
     export let slug: string;
 </script>
 
-<a href="/projects/{slug}" class="no-underline">
-    <div class="relative h-72 w-72 group transition-transform hover:transform hover:-translate-x-1 hover:-translate-y-1">
-
-        <div class="absolute group-hover:left-4 group-hover:top-4 transition-all left-2 top-2 h-[95%] w-[95%] outline-dashed outline-offset-2 outline-[4px] outline-blue-500"></div>
+<a
+    href="/projects/{slug}"
+    class="no-underline">
+    <div class="group relative h-72 w-72 transition-transform hover:-translate-x-1 hover:-translate-y-1 hover:transform">
+        <div class="absolute left-2 top-2 h-[95%] w-[95%] outline-dashed outline-[4px] outline-offset-2 outline-blue-500 transition-all group-hover:left-4 group-hover:top-4"></div>
 
         <!-- PORTFOLIO ITEMS -->
-        <div class="relative flex h-72 w-72 justify-center border-[1.5px] border-blue-500 bg-white shadow-frame">
-
+        <div class="shadow-frame relative flex h-72 w-72 justify-center border-[1.5px] border-blue-500 bg-white">
             <!-- PORTFOLIO HOVER ITEMS -->
-            <div class="absolute left-1/2 top-1/2 flex h-full w-full bg-blue-500 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 px-4 opacity-0 group-hover:opacity-100 transition-opacity">
-                <h2 class="text-center text-2xl heading-font text-white transition md:text-3xl">
+            <div class="absolute left-1/2 top-1/2 flex h-full w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 bg-blue-500 px-4 opacity-0 transition-opacity group-hover:opacity-100">
+                <h2 class="heading-font text-center text-2xl text-white transition md:text-3xl">
                     {title}
                 </h2>
                 <p class="text-center text-lg text-white md:text-xl">
@@ -25,21 +25,29 @@
 
             <!-- PORTFOLIO IMAGE -->
             {#if image}
-                <img
-                    class="object-cover w-full brightness-50 transition-opacity group-hover:opacity-0"
-                    src={image}
-                    alt={title} />
+                {#if title == "Muxly" || title == "Lapse"}
+                    <img
+                        class="w-full bg-blue-500 object-contain brightness-100 transition-opacity group-hover:opacity-0"
+                        src={image}
+                        alt={title} />
+                {:else}
+                    <img
+                        class="w-full object-cover brightness-50 transition-opacity group-hover:opacity-0"
+                        src={image}
+                        alt={title} />
+                {/if}
             {:else}
-                <div class="w-full h-full bg-blue-500/30 dark:bg-blue-500/40 animate-pulse flex items-center justify-center">
-                    <span class="text-white/80 text-sm font-light">Loading...</span>
+                <div class="flex h-full w-full animate-pulse items-center justify-center bg-blue-500/30 dark:bg-blue-500/40">
+                    <span class="text-sm font-light text-white/80">Loading...</span>
                 </div>
             {/if}
 
-            <!-- PORTFOLIO TITLE -->
-            <h2 class="absolute left-1/2 top-1/2 w-full heading-font group-hover:invisible group-hover:opacity-0 transition-opacity -translate-x-1/2 -translate-y-1/2 px-1 text-center text-2xl text-white md:text-3xl">
-                {title}
-            </h2>
-
+            {#if title != "Muxly" && title != "Lapse"}
+                <!-- PORTFOLIO TITLE -->
+                <h2 class="heading-font absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 px-1 text-center text-2xl text-white transition-opacity group-hover:invisible group-hover:opacity-0 md:text-3xl">
+                    {title}
+                </h2>
+            {/if}
         </div>
     </div>
 </a>
